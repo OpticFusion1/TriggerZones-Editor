38dea67e08d4967c7bfd52e7b23f4fdb15c3948f
OpticFusion1 <37254722+OpticFusion1@users.noreply.github.com>
2026-03-04 22:26:37 +0100

    Initial Commit

---
+++ src/main/java/optic_fusion1/tzeditor/TZEditor.java
@@ -0,0 +1,11 @@
+package optic_fusion1.tzeditor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TZEditor extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+}
